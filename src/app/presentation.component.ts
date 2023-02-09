eb0b46894a0aff461e8136521ab7c2d0cc68c87d
clemparpa <clem.parpaillon@gmail.com>
2023-02-09 23:39:50 +0100

    ui fix

--- src/app/presentation.component.ts
+++ src/app/presentation.component.ts
@@ -6,7 +6,7 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [CommonModule],
   template: `
-    <section class="bg-gray-900 text-white">
+    <section class="bg-gray-900 text-white min-h-screen">
       <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
         <div class="mx-auto max-w-lg text-center">
           <h2 class="text-3xl font-bold sm:text-4xl">
